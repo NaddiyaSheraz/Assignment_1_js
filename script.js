381422f0550a9d1ac1509449ba1e9308b5e77731
Naddiya Sheraz <naddiyasheraz18@gmail.com>
2024-08-06 10:55:26 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// >>>>>1.Declare a variable called age & assign to it your age. Show >>>>>
+// your age in an alert box.
+var BirthYear= "I am 19 years old";
+alert (BirthYear);
+
+// // >>>>>2.Declare & initialize a variable to keep track of how many 
+// times a visitor has visited a web page. Show his/her 
+// number of visits on your web page. For example: “You 
+// have visited this site N times”.>>>>>
+alert ("you have visited this site 4 times");
+
+
+// >>>>>>>>>>3.Declare a variable called birthYear & assign to it your 
+// birth year. Show the following message in your browser: >>>>>>>>
+var BirthYear = 2004;
+document .write("My Birth Year is 2004" + "<br/>" + "Data type of my declared variable is number");
+
+
+// >>>>>>4.A visitor visits an online clothing store 
+// www.xyzClothing.com . Write a script to store in variables 
+// the following information: 
+// b. Product title 
+// c. Quantity i.e. how many products a visitor wants to 
+// order 
+// Show the following message in your browser: “John 
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”. >>>>>>
+var VisitorName = "Naddiya Sheraz";
+var ProductTitle = "T-shirts";
+var quantity = 5;
+document.write("<b>" + VisitorName + "</b>" + " " + "ordered" + " " + "<b>" +
+     quantity + "</b>" + " " + "<b>" + ProductTitle + "</b>" + " " + "on XYZ clothing store");
